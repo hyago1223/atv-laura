eab70da4214b5553be68b52e83a70ebd22fafcd0
hyago1223 <hyago9433@gmail.com>
2025-12-11 21:57:26 -0300

    1.0.3.2

--- database/mysql.js
+++ database/mysql.js
@@ -1,12 +1,18 @@
 import { Sequelize } from "sequelize";
 import pg from "pg";
 
-const EXTERNAL_DATABASE_URL = 'postgresql://as:HMWzLvTKs3MSlpTFE41PZ2jPGRD3RzIG@dpg-d4tmaaumcj7s7382h8sg-a.oregon-postgres.render.com/a_t0t4'; 
+const EXTERNAL_DATABASE_URL = 'postgresql://as:HMWzLvTKs3MSlpTFE41PZ2jPGRD3RzIG@dpg-d4tmaaumcj7s7382h8sg-a.oregon-postgres.render.com/a_t0t4';
 
 const postgres = new Sequelize(EXTERNAL_DATABASE_URL, {
     dialect: 'postgres',
     dialectModule: pg,
 
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false 
+        }
+    }
 });
 
 export default postgres;
